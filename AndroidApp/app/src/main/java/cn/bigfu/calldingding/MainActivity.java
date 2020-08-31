4e2212ea6b042987eaa8215580cc15b80516651b
furunhao <fu@ns.sc.cn>
2020-08-31 13:30:25 +0800

    second

--- AndroidApp/app/src/main/java/cn/bigfu/calldingding/MainActivity.java
+++ AndroidApp/app/src/main/java/cn/bigfu/calldingding/MainActivity.java
@@ -1,13 +1,27 @@
 package cn.bigfu.calldingding;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn;
+        btn = this.findViewById(R.id.button1);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO Auto-generated method stub
+                setTitle("button1 被用户点击了");
+                Log.i("widgetDemo", "button1 被用户点击了。");
+
+            }
+        });
     }
 }
---
+++ Bluetooth/README.md
@@ -0,0 +1,2 @@
+# dingTalkRemoteCheckIn
+ 基于多种方式的钉钉远程打卡
